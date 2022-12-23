52f048f4ffd43bc975296e2babdc9566acf17cea
Siti Indiyati <sitiindiyati20@gmail.com>
2022-12-23 12:41:44 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Siti Indiyati";
+    $umur = "20";
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun,<br>";
+        
+    $namakakak = "Siti Kundriyati";
+    $umurkakak = "24";
+
+    echo "Nama kakak saya <strong>$namakakak</strong>, kakak saya berusia $umurkakak tahun<br>";
+
+    $selisihUmur = $umur - $umurkakak;
+    echo "Selisih usia saya dan kakak saya adalah 4 tahun $selisihUmur tahun.";
